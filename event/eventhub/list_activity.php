588f132ae7a58c96fe7c6946f2d35ab5fe4fef0f
InTheForest <104747445+slzzInTheForest@users.noreply.github.com>
2022-09-15 01:32:56 +0800

    修复特典活动和问卷的显示问题

---
+++ event/eventhub/list_activity.php
@@ -0,0 +1,11 @@
+<?php
+$cu = curl_init();
+$url = 'https://'.$_SERVER['SERVER_NAME'].'/event/eventhub/list_activity?'.$_SERVER["QUERY_STRING"];
+curl_setopt($cu, CURLOPT_URL, $url);
+curl_setopt($cu, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($cu, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($cu, CURLOPT_SSL_VERIFYHOST, false);
+$data = curl_exec($cu);
+curl_close($cu);
+echo $data;
+?>
