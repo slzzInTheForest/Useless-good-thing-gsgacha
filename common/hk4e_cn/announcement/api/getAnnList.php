1fd066d627fd27faa49041ac66f35aa49d35f892
InTheForest <104747445+slzzInTheForest@users.noreply.github.com>
2022-09-15 01:15:33 +0800

    公告适配国际服

--- common/hk4e_cn/announcement/api/getAnnList.php
+++ common/hk4e_cn/announcement/api/getAnnList.php
@@ -1,6 +1,6 @@
 <?php
 $cu = curl_init();
-$url = 'https://hk4e-api.mihoyo.com/common/hk4e_cn/announcement/api/getAnnList?'.$_SERVER["QUERY_STRING"];
+$url = 'https://'.$_SERVER['SERVER_NAME'].'/common/hk4e_cn/announcement/api/getAnnList?'.$_SERVER["QUERY_STRING"];
 curl_setopt($cu, CURLOPT_URL, $url);
 curl_setopt($cu, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($cu, CURLOPT_SSL_VERIFYPEER, false);
@@ -8,3 +8,4 @@ curl_setopt($cu, CURLOPT_SSL_VERIFYHOST, false);
 $data = curl_exec($cu);
 curl_close($cu);
 echo $data;
+?>
