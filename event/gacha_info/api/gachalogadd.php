f796e5ff24d7824ac664c1a1486def3be588fa57
InTheForest <104747445+slzzInTheForest@users.noreply.github.com>
2022-09-14 09:54:59 +0800

    优化数据生成速度

--- event/gacha_info/api/gachalogadd.php
+++ event/gacha_info/api/gachalogadd.php
@@ -1,9 +1,11 @@
 <?php
 include ('../../../config.php');
 $gettime = date("Y/m/d H:i:s");
-if ($_GET['key'] == $key || $key == ""){
+if ( $_GET['key'] == $key || $key == ""){
     //连接数据库
-    $conn=mysqli_connect($servername, $username, $password, $dbname);
+    $mysql=mysqli_connect($servername, $username, $password, $dbname);
+    //关闭自动提交
+    mysqli_autocommit($mysql,FALSE);
     if (mysqli_connect_errno()) {
         die("Connection failed: " . mysqli_connect_error());
     } else{
@@ -35,21 +37,24 @@ if ($_GET['key'] == $key || $key == ""){
                 $time = date("Y-m-d H:i:s");
             }if ($quantity == "" || is_numeric($quantity) != 1){
                 $sql = "INSERT INTO gacha (authkey, item_type,rank_type,name,time,gacha_type,uid)VALUES ('$authkey', '$item_type', '$rank_type' ,'$name','$time','$gacha_type','$uid')";
-                mysqli_query($conn, $sql);
+                mysqli_query($mysql, $sql);
+                mysqli_commit($mysql);
                 echo "插入成功！请求时间:$gettime,共插入一条数据 <br> 名字:$name,authkey:$authkey,物品类型:$item_type,时间:$time,gacha_type:$gacha_type,uid:$uid";
             }else{
                 echo "正在插入数据中...请稍后！<br>";
                 for ($fc = 0;$fc <= $quantity - 1; $fc++ ){
                     $sql = "INSERT INTO gacha (authkey, item_type,rank_type,name,time,gacha_type,uid)VALUES ('$authkey', '武器', '3' ,'黑缨枪','$time','$gacha_type','$uid')";
-                    mysqli_query($conn, $sql);
+                    mysqli_query($mysql, $sql);
                 }
                 $sql = "INSERT INTO gacha (authkey, item_type,rank_type,name,time,gacha_type,uid)VALUES ('$authkey', '$item_type', '$rank_type' ,'$name','$time','$gacha_type','$uid')";
-                mysqli_query($conn, $sql);
+                mysqli_query($mysql, $sql);
+                //提交数据
+                mysqli_commit($mysql);
                 echo "插入成功！请求时间:$gettime,共插入$quantity 条数据，其中五星为 <br> 名字:$name,authkey:$authkey,物品类型:$item_type,时间:$time,gacha_type:$gacha_type,uid:$uid";
             }
 
         }
-        mysqli_close($conn);
+        mysqli_close($mysql);
     }
 
 }
