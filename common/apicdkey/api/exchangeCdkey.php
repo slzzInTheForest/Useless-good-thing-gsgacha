b83951e2c3657d9d6fb160d996d6985aa28e2591
InTheForest <104747445+slzzInTheForest@users.noreply.github.com>
2022-09-17 00:13:37 +0800

    添加12位兑换码自动提交官方服务器功能

--- common/apicdkey/api/exchangeCdkey.php
+++ common/apicdkey/api/exchangeCdkey.php
@@ -2,36 +2,48 @@
 include ('../../../config.php');
 
 $cdkey = $_GET['cdkey'] ;
-if($Case_sensitive == "false" || $Case_sensitive == "") {
-    $scdkey = strtolower($cdkey);
-}
-//数据库
-$mysql=mysqli_connect($servername, $username, $password, $dbname);
-$result = mysqli_query($mysql, "SELECT * FROM cdkey WHERE cdkey='$cdkey'");
-$row = mysqli_fetch_array($result) ;
-if (mysqli_num_rows($result) > 0){
-    $msg = $row["msg"] ;
-    /*
-    if ($row["retcode"] == ""){
-        $retcode = -2016 ;
-    }else{
-        $retcode = $row["retcode"] ;
-    }
-    */
-    $res = array(
-        'data'=>null,
-        'message'=>"$msg",
-        'retcode'=>-2016
-    );
+if ($cdkey12 == "true" && strlen($cdkey) == 12){
+    $cu = curl_init();
+    $url = 'https://' . $_SERVER['HTTP_HOST'] . '/common/apicdkey/api/exchangeCdkey?' . $_SERVER["QUERY_STRING"];
+    curl_setopt($cu, CURLOPT_URL, $url);
+    curl_setopt($cu, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($cu, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($cu, CURLOPT_SSL_VERIFYHOST, false);
+    $data = curl_exec($cu);
+    curl_close($cu);
+    echo $data;
 }else{
-    $res = array(
-        'retcode'=>0,
-        'message'=>"OK",
-        'data'=>array(
-            'msg'=>"兑换成功",
-            'special_shipping'=>0
-        )
-    );
-}
+    if($Case_sensitive == "false" || $Case_sensitive == "") {
+        $scdkey = strtolower($cdkey);
+    }
+//数据库
+    $mysql=mysqli_connect($servername, $username, $password, $dbname);
+    $result = mysqli_query($mysql, "SELECT * FROM cdkey WHERE cdkey='$cdkey'");
+    $row = mysqli_fetch_array($result) ;
+    if (mysqli_num_rows($result) > 0){
+        $msg = $row["msg"] ;
+        /*
+        if ($row["retcode"] == ""){
+            $retcode = -2016 ;
+        }else{
+            $retcode = $row["retcode"] ;
+        }
+        */
+        $res = array(
+            'data'=>null,
+            'message'=>"$msg",
+            'retcode'=>-2016
+        );
+    }else{
+        $res = array(
+            'retcode'=>0,
+            'message'=>"OK",
+            'data'=>array(
+                'msg'=>"兑换成功",
+                'special_shipping'=>0
+            )
+        );
+    }
 
-echo json_encode($res,JSON_UNESCAPED_UNICODE);
+    echo json_encode($res,JSON_UNESCAPED_UNICODE);
+}
